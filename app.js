57dc7560c968aede1e38acdafe00dc35ed7e1609
AbhijithAbhi001 <abhijith513226@gmail.com>
2025-10-28 12:35:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// let user = 'yash';
+// console.log(user)
+// let items =[ 'milk', 'bread','butter','apple'];
+// console.log(items)
+// items[1]='muttapapps'
+// console.log(items)
+// items.push('mangoes','orange','pinapple')
+// items.pop()
+// items.pop()
+// items.splice(1,1,'mango')
+// console.log(items)
+// let user ={
+//     name:'Abcd',
+//     Age: 34,
+//     address:'Mumbai',
+//     email: 'abcd@gmail.com',
+//     hobbies:['teaching','reading','cooking']
+// }
+// // console.log(user.hobbies[2])
+// user.name ='ram'
+// user.phone = '0987654321'
+// // console.log(user.phone)
+// delete user.Age
+// console.log(user)
+// console.log(typeof user)
+// let users =[
+//     {name:'yash', age:24, address:'mumbai'},
+//     {name:'Abhi', age:18, address:'Bangalore'},
+//     {name:'Ram', age:20, address:'chennai'},
+//     {name:'Krish', age:17, address:'Goa'},
+//     {name:'Gopal', age:15, address:'kerala'},
+//     {name:'Nandu', age:15, address:'kerala'},
+//     {name:'Don', age:15, address:'kerala'},
+//     {name:'Vijay', age:15, address:'kerala'},
+//     {name:'Arun', age:15, address:'kerala'},
+//     {name:'Shyam', age:15, address:'kerala'},
+//     {name:'Addarsh', age:15, address:'kerala'},
+//     {name:'Praveen', age:15, address:'kerala'},
+//     {name:'Sasi', age:15, address:'kerala'},
+//     {name:'Sangu', age:15, address:'kerala'},
+//     {name:'ROhith', age:15, address:'kerala'},
+//     {name:'Rohan', age:15, address:'kerala'},
+// ]
+// let random = Math.random()
+// let random=Math.floor(Math.random()*100)
+// let random= Math.floor(Math.random()*10)
+// let random=Math.floor(Math.random()*100000)
+// console.log(random)
+// let random = Math.floor(Math.random()*users.length)
+// console.log(users.length)
+// console.log(users[random].name)
+
